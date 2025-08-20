6af2ddbb46f651a2e0c9206900a7af511b4f9eda
dinkeshjain <dinkeshjainjee@gmail.com>
2025-08-21 02:11:06 +0530

    update

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ app.use(cors({
     'https://anuadmin.bah.in',
     'https://anuevaluator.bah.in',
     'https://anustudent.bah.in',
+
+    'http://anuadmin.bah.in',
+    'http://anuevaluator.bah.in',
+    'http://anustudent.bah.in',
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
@@ -45,6 +49,7 @@ app.options('*', (req, res) => {
   const allowedOrigins = [
     'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175',
     'https://anuadmin.bah.in', 'https://anuevaluator.bah.in', 'https://anustudent.bah.in',
+    'http://anuadmin.bah.in', 'http://anuevaluator.bah.in', 'http://anustudent.bah.in'
   ];
   
   if (allowedOrigins.includes(origin)) {
